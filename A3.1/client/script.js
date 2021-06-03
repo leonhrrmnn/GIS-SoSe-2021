3b680935ef918b21d575b2f2d8b038c3c8b636a4
leonhrrmnn <80950606+leonhrrmnn@users.noreply.github.com>
2021-06-03 16:47:27 +0200

    A3.1 js

--- A3.1/client/script.js
+++ A3.1/client/script.js
@@ -1,7 +1,7 @@
 "use strict";
 var P_3_1;
 (function (P_3_1) {
-    async function daten(_url) {
+    async function daten() {
         let formData = new FormData(document.forms[0]);
         console.log(formData.get("name"));
         for (let entry of formData) {
@@ -9,21 +9,19 @@ var P_3_1;
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
         }
+        let url = "https://testleonhrrmnn.herokuapp.com";
         let session = new URLSearchParams(formData);
         console.log(session.toString());
-        _url = _url + "?" + session.toString();
-        console.log(_url);
-        let response = await fetch(_url);
+        url = url + "?" + session.toString();
+        console.log(url);
+        let response = await fetch(url);
         console.log(response);
         let ausgabe = await response.text();
         console.log(ausgabe);
         let zurückgeben = document.getElementById("display");
         zurückgeben.innerText = ausgabe;
     }
-    async function hndData() {
-        daten("https://testleonhrrmnn.herokuapp.com");
-    }
     let button = document.getElementById("button");
-    button.addEventListener("click", hndData);
+    button.addEventListener("click", daten);
 })(P_3_1 || (P_3_1 = {}));
 //# sourceMappingURL=script.js.map
